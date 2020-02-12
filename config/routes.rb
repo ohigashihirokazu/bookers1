097a1bb191b198dd073af95d7e8aaff008f9e782
ohigashihirokazu <hirokazudesu.1222@gmail.com>
2020-02-12 20:31:29 +0900

    bookers1

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'books#top'
+
+ 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books
+
+ 
+
+   
+end
